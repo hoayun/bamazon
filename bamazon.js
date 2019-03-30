be359c26ba974831e1724f303c3c4bfc0d8d3f76
Chris Hoayun <45009760+hoayun@users.noreply.github.com>
2019-03-29 23:27:58 -0400

    working part 1

--- bamazon.js
+++ bamazon.js
@@ -19,19 +19,19 @@ connection.connect(function (err) {
   if (err) throw err;
   start();
 })
-function start(){
-  connection.query("SELECT * FROM `products`", function(err, results) {
+function start() {
+  connection.query("SELECT * FROM `products`", function (err, results) {
     if (err) throw err;
     var table = new Table({
-        head: ['ID', 'Product Name', 'Department', 'Price', 'Stock Quantity']
+      head: ['ID', 'Product Name', 'Department', 'Price', 'Stock Quantity']
       , colWidths: [5, 20, 15, 10, 17]
     });
-     
+
     // table is an Array, so you can `push`, `unshift`, `splice` and friends
     for (i = 0; i < results.length; i++) {
-        table.push(
-            [results[i].item_id, results[i].product_name, results[i].department_name, results[i].price, results[i].stock_quantity]
-        );
+      table.push(
+        [results[i].item_id, results[i].product_name, results[i].department_name, results[i].price, results[i].stock_quantity]
+      );
     }
     console.log("success");
 
@@ -63,13 +63,20 @@ function start(){
           console.log(fields[0].stock_quantity);
           z = (parseInt(fields[0].stock_quantity) - y);
           console.log(z);
-       connection.query("UPDATE `products` SET `stock_quantity` = '"+z+"' WHERE `item_id` = " + x, function(err, results) {
-        //  if (err) throw err;
-        connection.end();})
-
-  
+          cost = ((parseInt(fields[0].price)) * y);
+          if (z > 0) {
+            connection.query("UPDATE `products` SET `stock_quantity` = '" + z + "' WHERE `item_id` = " + x, function (err, results) {
+                if (err) throw err;
+                console.log("Your purchase today cost " + cost + "$.")
+              connection.end();
+            })
+          }
+          else {
+            console.log("Insufficient stock!!!!");
+            connection.end();
+          }
         })
       })
-      
+
   }
 }
